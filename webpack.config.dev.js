e81f68c8bfece75a21491a88fcd147a9f19c3df4
chnpeople <53518477+chnpeople@users.noreply.github.com>
2020-03-31 00:00:16 +0800

    解决htmlWebpackPlugin路径问题

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,6 +1,6 @@
 const path = require('path'); //nodeJs的基本包
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   // 入口文件，用绝对路径，保证我们不因为路径发生错误
   //path.join(__dirname, 'src/index.js')中__dirname表示当前文件的路径，path.join就是将当前文件的路径跟'src/index.js'拼接起来，形成一个绝对路径
