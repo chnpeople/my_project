87b93e2d0c7a2d5b16b46858da031159132b523e
chnpeople <a289815438@qq.com>
2020-12-26 00:15:15 +0800

    二进制流

---
+++ src/components/upload2.vue
@@ -0,0 +1,29 @@
+<template>
+  <input type="file" @change="getFile($event)">
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+        getFile(e) {
+            console.log(e.target.files[0]);
+            let blob = new Blob([e.target.files[0]]);
+            let url = URL.createObjectURL(blob);
+            console.log(url);
+            const a = document.createElement('a');
+            a.download = '测试.xlsx';
+            a.href = url;
+            a.click();
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
