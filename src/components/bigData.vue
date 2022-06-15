c387bfead9d0db569ed7a1ace748745ee7d22579
Tim <tim@Tims-MacBook-Pro.local>
2022-06-15 17:08:22 +0800

    custom table

--- src/components/bigData.vue
+++ src/components/bigData.vue
@@ -2,15 +2,123 @@
   <div>
     <el-button @click="getData">10万条数据</el-button>
     <div id="container"></div>
+
+    <div style="width: 500px; height: 800px;">
+      <table
+        id="my-table"
+        class="my-table"
+        cellspacing="0"
+        border="0"
+        bordercolor="#ccc"
+      ></table>
+    </div>
+
+    <div style="width: 500px; height: 800px;">
+      <table class="my-table" cellspacing="0" border="0" bordercolor="#ccc">
+        <tbody>
+          <tr>
+            <td>姓名</td>
+            <td>xxx</td>
+            <td>性别</td>
+            <td>男</td>
+          </tr>
+          <tr>
+            <td colspan="2">地址</td>
+            <td colspan="2">广东省深圳市宝安区宝安机场</td>
+          </tr>
+          <tr>
+            <td rowspan="6">航司</td>
+            <td rowspan="3">大陆</td>
+            <td colspan="2">南航</td>
+          </tr>
+          <tr>
+            <!-- <td>航司</td> -->
+            <!-- <td>大陆</td> -->
+            <td colspan="2">东航</td>
+          </tr>
+          <tr>
+            <!-- <td>航司</td> -->
+            <!-- <td>大陆</td> -->
+            <td colspan="2">国航</td>
+          </tr>
+          <tr>
+            <!-- <td>航司</td> -->
+            <td rowspan="2">亚洲</td>
+            <td colspan="2">阿联酋航空</td>
+          </tr>
+          <tr>
+            <!-- <td>航司</td> -->
+            <!-- <td>亚洲</td> -->
+            <td colspan="2">日本航空</td>
+          </tr>
+          <tr>
+            <!-- <td>航司</td> -->
+            <td>国外</td>
+            <td colspan="2">泛美航空</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      table: [
+        ['姓名', 'xxx', '性别', '男'],
+        ['地址', '广东省深圳市宝安区宝安机场'],
+        ['航司', '大陆', '南航'],
+        ['航司', '大陆', '东航'],
+        ['航司', '大陆', '国航'],
+        ['航司', '亚洲', '阿联酋航空'],
+        ['航司', '亚洲', '日本航空'],
+        ['航司', '国外', '泛美航空'],
+      ],
+    };
+  },
+  created() {},
+  mounted() {
+    this.hanlderTabele();
   },
   methods: {
+    hanlderTabele() {
+      const mytable = document.getElementById('my-table');
+      console.log('mytable', mytable);
+      const tbody = document.createElement('tbody');
+      let span = 1;
+      this.table.forEach((item, idx) => {
+        span += 1;
+        const tr = document.createElement('tr');
+        let tdLength = item.length;
+        item.forEach((item2, index) => {
+          const td = document.createElement('td');
+          td.setAttribute(
+            'style',
+            'border-bottom: 1px solid #ccc; border-right: 1px solid #ccc; padding: 5px; text-align: center;'
+          );
+          if (item.length === 2) {
+            td.setAttribute('colspan', 2);
+          } else if (item.length === 3 && index === 2) {
+            td.setAttribute('colspan', 2);
+          }
+          if (idx != 0 && index === 0 && item2 == this.table[idx - 1][index]) {
+            return;
+          }
+          span += 1;
+          if (idx != 0 && index === 0 && item2 == this.table[idx + 1][index]) {
+            span += 1;
+            td.setAttribute('rowspan', span);
+          }
+          tr.appendChild(td);
+          td.innerHTML = item2;
+        });
+        tbody.appendChild(tr);
+      });
+      mytable.appendChild(tbody);
+    },
+
     async getData() {
       console.log(new Date().getTime());
       const container = document.getElementById('container');
@@ -80,4 +188,17 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.my-table {
+  width: 100%;
+  margin-top: 20px;
+  border-left: 1px solid #ccc;
+  border-top: 1px solid #ccc;
+  td {
+    border-bottom: 1px solid #ccc;
+    border-right: 1px solid #ccc;
+    padding: 5px;
+    text-align: center;
+  }
+}
+</style>
