e3f405f3427594462c68820441236101112a01eb
chnpeople <a289815438@qq.com>
2022-07-03 20:54:19 +0800

    test1

--- src/views/home.vue
+++ src/views/home.vue
@@ -13,7 +13,7 @@
       >
         <el-submenu index="1">
           <template slot="title">
-            <span>项目</span>
+            <span>项目1</span>
           </template>
           <el-menu-item-group>
             <el-menu-item :index="item.path" v-for="(item, index) in menuList" :key="index">{{item.title}}</el-menu-item>
